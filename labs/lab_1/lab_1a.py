1898d878a70fdec31eeeebd86a39d94fde0ce478
aditikhiste <aditi.khiste2027@gmail.com>
2026-02-15 13:25:52 -0500

    change

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,7 +1,5 @@
 """
-
 lab_1a.py
-This is to simulate a change made on a robot: robot_speed = 5 # m/s
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
@@ -15,7 +13,7 @@ def main():
     print(f"{name}, Welcome to the CSS course!")
     print("Hi! My name is Aditi Khiste and I am a junior from Massachusetts. Some fun facts about me are that I play badminton and enjoy making mandala art in my free time. I am looking forward to taking this course and meeting everyone!")
     print("test")
-    print("change made")
+    print()
 
 if __name__ == "__main__":
     main()
