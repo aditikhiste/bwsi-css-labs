24270ff6b15d8e511d856da53320a20a943b381b
aditikhiste <aditi.khiste2027@gmail.com>
2026-02-15 06:57:52 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Aditi Khiste" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi! My name is Aditi Khiste and I am a junior from Massachusetts. Some fun facts about me are that I play badminton and enjoy making mandala art in my free time. I am looking forward to taking this course and meeting everyone!")
 
 if __name__ == "__main__":
     main()
